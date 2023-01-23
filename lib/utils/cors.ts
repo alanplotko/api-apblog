dd8c038d6c7a2859211dc312b356fa8c234d8dcb
Alan Plotko <5394528+alanplotko@users.noreply.github.com>
2023-01-23 18:05:55 -0500

    Resolve build errors
    - Update prisma schema location
    - Remove unused dependency

--- lib/utils/cors.ts
+++ lib/utils/cors.ts
@@ -1,16 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { UnauthorizedException } from "next-api-decorators";
 import NextCors from "nextjs-cors";
 
 export const corsHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const cors = await NextCors(req, res, {
+  await NextCors(req, res, {
     // Options
     methods: ['GET', 'PUT', 'OPTIONS'],
     origin: process.env.ORIGINS,
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
-
-  if (cors instanceof Error) {
-    throw new UnauthorizedException();
-  }
 }
